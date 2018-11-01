9e6ba1900cecdd62eb904b9812517ba50185f71b
Cameron Cooper <zdot101@gmail.com>
2018-11-01 18:17:28 -0400

    Added migration for subtasks of a task

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Task;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +14,12 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/task',function(Request $request) {
+    //
+});
+
+Route::delete('/task/{id}', function($id){
+    //
 });
